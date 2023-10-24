098843f56ee64f5bbe6a2458be0153650c137db0
m4tiss <mateusz039@op.pl>
2023-10-24 15:04:37 +0200

    Add SCSS

--- settings.php
+++ settings.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8"/>
     <title>Step in Style</title>
     <link rel="stylesheet" href="styles/index.css">
+    <link rel="stylesheet" href="styles/navbar.css">
     <link rel="stylesheet" href="styles/footer.css">
 </head>
 <body>
