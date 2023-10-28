4adb8fb1ee0c3edd473577f310065d79d3090f9f
m4tiss <mateusz039@op.pl>
2023-10-28 12:43:03 +0200

    Change file content

--- settings.php
+++ settings.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="styles/registration.css">
     <link rel="stylesheet" href="styles/clothes.css">
     <link rel="stylesheet" href="styles/product.css">
+    <link rel="stylesheet" href="styles/footwear.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
