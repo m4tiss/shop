65f357fc61ad33fcb6ba7fc3146f06d67a57edfb
m4tiss <mateusz039@op.pl>
2023-11-24 22:30:59 +0100

    add font from folder and change charset in

--- config.php
+++ config.php
@@ -6,6 +6,7 @@ $password = "praktyka";
 $database = "shop";
 
 $conn = new mysqli($servername, $username, $password, $database);
+$conn->set_charset("utf8mb4");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
--- settings.php
+++ settings.php
@@ -26,9 +26,6 @@
     <link rel="stylesheet" href="../styles/accountAdmin.css">
     <link rel="stylesheet" href="../styles/orderDetails.css">
     <link rel="stylesheet" href="../styles/stylesAdmin.css">
-<!--    <link rel="preconnect" href="https://fonts.googleapis.com">-->
-<!--    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>-->
-<!--    <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500;600;700;800;900&display=swap">-->
     <script src="scripts/registrationAndLogin.js"></script>
     <script src="scripts/filtration.js"></script>
     <script src="scripts/index.js"></script>
